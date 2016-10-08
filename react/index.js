df80f80031642e934fac8951dfb98ace78aca914
Cameron Bytheway <bytheway.cameron@gmail.com>
2016-10-07 16:44:45 -1000

    add main entry support

--- react/index.js
+++ react/index.js
@@ -3,10 +3,13 @@ var createElement = React.createElement;
 var Application = require('./components/application');
 
 module.exports = function(config) {
+  var entry = config.entry;
   return function createApp(src, props) {
-    return createElement(Application, Object.assign({
+    var app = createElement(Application, Object.assign({
       src: src,
       props: props
     }, config));
+    if (entry) app = createElement(entry, null, app);
+    return app;
   };
 };
